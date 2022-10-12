2bda466809af770737750383d85464c96167620c
igorv404 <igorvilkov404@gmail.com>
2022-10-12 15:48:39 +0300

    add code

--- src/io/igorv404/Main.java
+++ src/io/igorv404/Main.java
@@ -1,12 +1,12 @@
 package io.igorv404;
 
 import io.igorv404.models.FileData;
-import io.igorv404.models.Wedding;
+import io.igorv404.models.Wedd;
 
 public class Main {
   public static void main(String[] args) {
     FileData fileData = new FileData();
-    Wedding wedding = new Wedding(fileData.getAllPairs(fileData.countOfPairs()));
-    System.out.println(wedding.getAllPossiblePairs());
+    Wedd wedd = new Wedd(fileData.getAllPairs(fileData.countOfPairs()));
+    System.out.println(wedd.getAllPossiblePairs());
   }
 }
